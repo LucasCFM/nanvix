033f60512c199e9b5345dcc308b9c6f996a87a18
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2019-06-11 22:06:50 -0300

    FIFO first try

--- src/kernel/pm/sched.c
+++ src/kernel/pm/sched.c
@@ -62,59 +62,83 @@ PUBLIC void resume(struct process *proc)
 /**
  * @brief Yields the processor.
  */
-PUBLIC void yield(void)
-{
-	struct process *p;    /* Working process.     */
-	struct process *next; /* Next process to run. */
+// PUBLIC void yield(void)
+// {
+// 	struct process *p;    /* Working process.     */
+// 	struct process *next; /* Next process to run. */
 
-	/* Re-schedule process for execution. */
+// 	/* Re-schedule process for execution. */
+// 	if (curr_proc->state == PROC_RUNNING)
+// 		sched(curr_proc);
+
+// 	/* Remember this process. */
+// 	last_proc = curr_proc;
+
+// 	/* Check alarm. */
+// 	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+// 	{
+// 		/* Skip invalid processes. */
+// 		if (!IS_VALID(p))
+// 			continue;
+		
+// 		/* Alarm has expired. */
+// 		if ((p->alarm) && (p->alarm < ticks))
+// 			p->alarm = 0, sndsig(p, SIGALRM);
+// 	}
+
+// 	/* Choose a process to run next. */
+// 	next = IDLE;
+// 	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+// 	{
+// 		/* Skip non-ready process. */
+// 		if (p->state != PROC_READY)
+// 			continue;
+		
+// 		/*
+// 		 * Process with higher
+// 		 * waiting time found.
+// 		 */
+// 		if (p->counter > next->counter)
+// 		{
+// 			next->counter++;
+// 			next = p;
+// 		}
+			
+// 		/*
+// 		 * Increment waiting
+// 		 * time of process.
+// 		 */
+// 		else
+// 			p->counter++;
+// 	}
+	
+// 	/* Switch to next process. */
+// 	next->priority = PRIO_USER;
+// 	next->state = PROC_RUNNING;
+// 	next->counter = PROC_QUANTUM;
+// 	switch_to(next);
+// }
+
+
+/**
+ * @brief Yields the processor, using the FIFO scheduler
+ */
+PUBLIC void yield(void) {
+	struct process *p;    /* Working process. Gonna be the first scheduled  */
+
+	/* Re-schedule current process for execution if it is running. Not run it */
 	if (curr_proc->state == PROC_RUNNING)
 		sched(curr_proc);
-
-	/* Remember this process. */
-	last_proc = curr_proc;
-
-	/* Check alarm. */
-	for (p = FIRST_PROC; p <= LAST_PROC; p++)
-	{
-		/* Skip invalid processes. */
-		if (!IS_VALID(p))
-			continue;
-		
-		/* Alarm has expired. */
-		if ((p->alarm) && (p->alarm < ticks))
-			p->alarm = 0, sndsig(p, SIGALRM);
-	}
-
-	/* Choose a process to run next. */
-	next = IDLE;
-	for (p = FIRST_PROC; p <= LAST_PROC; p++)
-	{
-		/* Skip non-ready process. */
-		if (p->state != PROC_READY)
-			continue;
-		
-		/*
-		 * Process with higher
-		 * waiting time found.
-		 */
-		if (p->counter > next->counter)
-		{
-			next->counter++;
-			next = p;
-		}
-			
-		/*
-		 * Increment waiting
-		 * time of process.
-		 */
-		else
-			p->counter++;
-	}
 	
-	/* Switch to next process. */
-	next->priority = PRIO_USER;
-	next->state = PROC_RUNNING;
-	next->counter = PROC_QUANTUM;
-	switch_to(next);
+	p = FIRST_PROC;
+	while(p->counter != 0) {
+		// Iterate untill gets the first process that did not run
+		p = p++;
+	}
+
+	p->counter = 1;
+	p->state = PROC_RUNNING;
+	switch_to(p);
 }
+
+
